943d819ab9efbaaeafbbc4a9e6d5a1a3aeafb591
ranstar74 <falconr1125@gmail.com>
2024-01-01 20:43:04 +0300

    Brighten window title if focused

--- imgui.cpp
+++ imgui.cpp
@@ -6685,9 +6685,10 @@ void ImGui::RenderWindowTitleBarContents(ImGuiWindow* window, const ImRect& titl
     }
     //if (g.IO.KeyShift) window->DrawList->AddRect(layout_r.Min, layout_r.Max, IM_COL32(255, 128, 0, 255)); // [DEBUG]
     //if (g.IO.KeyCtrl) window->DrawList->AddRect(clip_r.Min, clip_r.Max, IM_COL32(255, 128, 0, 255)); // [DEBUG]
-    PushStyleColor(ImGuiCol_Text, GetColorU32(ImGuiCol_Text, 0.85f));
+    bool focused = IsWindowFocused();
+    if (!focused) PushStyleColor(ImGuiCol_Text, GetColorU32(ImGuiCol_Text, 0.75f));
     RenderTextClipped(layout_r.Min, layout_r.Max, name, NULL, &text_size, style.WindowTitleAlign, &clip_r);
-    PopStyleColor();
+    if (!focused) PopStyleColor();
 }
 
 void ImGui::UpdateWindowParentAndRootLinks(ImGuiWindow* window, ImGuiWindowFlags flags, ImGuiWindow* parent_window)
